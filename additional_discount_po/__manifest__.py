e2dd346d0f830d6189ba177aea109856a6089579
Muhamad Syahril Aziz <111359449+Zizz4@users.noreply.github.com>
2023-08-29 23:33:12 +0700

    Update __manifest__.py

--- additional_discount_po/__manifest__.py
+++ additional_discount_po/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Additional discount for PO',
     'version': '16.0.1.1.0',
     'summary': 'Additional discount for PO',
-    'description': """" Insert an additional discount for PO""",
+    'description': """Insert an additional discount for PO""",
     'category': 'Inventory/Purchase',
     'author': 'Muhamad Syahril Aziz',
     'license': 'LGPL-3',
